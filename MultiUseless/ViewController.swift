c6d008e537cb6a4310aaa631c8993f48dd676512
stefanv.demoraes <stefanvargas3@gmail.com>
2018-08-15 14:13:48 -0300

    Phase 1
    Created sound nodes

---
+++ MultiUseless/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiUseless
+//
+//  Created by Stefan V. de Moraes on 15/08/2018.
+//  Copyright © 2018 Stefan V. de Moraes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
