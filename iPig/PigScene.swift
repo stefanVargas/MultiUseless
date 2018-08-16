9eaebb53b7084c384a44602e7df51fe03b43511f
Giovane Barreira <giovane_barreira@hotmail.com>
2018-08-16 18:35:49 -0300

    Added pig Sprites

--- iPig/PigScene.swift
+++ iPig/PigScene.swift
@@ -11,6 +11,8 @@ import SpriteKit
 
 class PigScene: SKScene {
     
+    
+    //create a function
     var tailBtn = SKSpriteNode()
     var bellyBtn = SKSpriteNode()
     var mouthBtn = SKSpriteNode()
@@ -19,8 +21,23 @@ class PigScene: SKScene {
     let happySound = SKAction.playSoundFileNamed("pigHappy", waitForCompletion: false)
     let scaredSound = SKAction.playSoundFileNamed("pigScared", waitForCompletion: false)
     
+    //MARK: Set sprite for Pig
+    private var dino = SKSpriteNode()
+    private var dinoWalkingFrame: [SKTexture] = []
+    
+    private var dinoJump = SKSpriteNode()
+    private var dinoJumpFrame: [SKTexture] = []
+    
+    private var dinoDie = SKSpriteNode()
+    private var dinoDieFrame: [SKSpriteNode] = []
+    
     
     override func didMove(to view: SKView) {
+        
+        buildAnimation(atlasName: "pig_inicio", nameOfSprites: "pig_inicio", arraySKTexture: &dinoWalkingFrame, currentNode: &dino)
+        animateDino()
+        
+        
         for node in self.children{
             if node.name == "tail"{
                 if let anyNode: SKSpriteNode = node as? SKSpriteNode {
@@ -30,7 +47,9 @@ class PigScene: SKScene {
             
             if node.name == "belly"{
                 if let anyNode2: SKSpriteNode = node as? SKSpriteNode{
+                    
                     bellyBtn = anyNode2
+                   
                 }
             }
 
@@ -53,6 +72,7 @@ class PigScene: SKScene {
             if bellyBtn.frame.contains(location){
                 print("belly")
                 run(happySound)
+                
             }
 
             if mouthBtn.frame.contains(location){
@@ -61,5 +81,72 @@ class PigScene: SKScene {
             }
         }
     }
+//
+//    func buildDino(atlasName: String, textureName: String){
+//        let dinoAnimatedAtlas = SKTextureAtlas(named: atlasName)
+//        var walkFrames:[SKTexture] = []
+//
+//        let numImages = dinoAnimatedAtlas.textureNames.count
+//        for i in 1...numImages{
+//            let dinoTextureName = textureName + "\(i)"
+//            walkFrames.append(dinoAnimatedAtlas.textureNamed(dinoTextureName))
+//        }
+//        dinoWalkingFrame = walkFrames
+//
+//        let firstFrameTexture = dinoWalkingFrame[0]
+//        dino = SKSpriteNode(texture: firstFrameTexture)
+//        dino.position = CGPoint(x: frame.midX, y: frame.midY)
+//        addChild(dino)
+//    }
+    
+    //funcao para rodar os sprites
+    func buildAnimation(atlasName: String, nameOfSprites: String, arraySKTexture: inout [SKTexture], currentNode: inout SKSpriteNode){
+        let walkAtlas = SKTextureAtlas(named: atlasName)
+        var walkFrames: [SKTexture] = []
+        let numImages = walkAtlas.textureNames.count
+        
+        
+        for i in 0..<numImages{
+            var textureName = nameOfSprites + "0\(i)"
+            if i >= 10 {
+                textureName = nameOfSprites + "\(i)"
+            }
+            
+            print(walkAtlas.textureNamed(textureName))
+            walkFrames.append(walkAtlas.textureNamed(textureName))
+        }
+        
+        arraySKTexture = walkFrames
+        
+        //Pega a primeira img do atlas e coloca numa var de textura
+        let firstFrameTexture = arraySKTexture[0]
+        currentNode = SKSpriteNode(texture: firstFrameTexture)
+        
+        currentNode.size = CGSize(width: 600, height: 600)
+        currentNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(currentNode)
+    }
+    
+    
+    func animateDino(){
+        dino.run(SKAction.repeatForever(
+            SKAction.animate(with: dinoWalkingFrame,
+                             timePerFrame: 0.1,
+                             resize: false,
+                             restore: true)),
+                 withKey:"dinoStop")
+    }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
